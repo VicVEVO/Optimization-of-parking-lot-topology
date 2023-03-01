6370d6b18fb35c5e366bc79aadefd0b477aff571
Victor <88597698+VicVEVO@users.noreply.github.com>
2023-03-01 13:08:24 +0100

    Update programme_principal.py

--- DERNIERE_BACKUP/programme_principal.py
+++ DERNIERE_BACKUP/programme_principal.py
@@ -15,7 +15,17 @@ if __name__ == '__main__':
     main()
 
 '''
-Idées à tester: Algo génétique progressif (N_itérations augmente au fur et à mesure)
-                Faire stopper la simulation dès qu'aucune voiture ne peut bouger >> faster
-Problèmes: croisement inutile
+
+Idées à tester:
+- Algo génétique progressif (N_itérations augmente au fur et à mesure)
+- Faire stopper la simulation dès qu'aucune voiture ne peut bouger >> faster
+
+Problèmes:
+- Croisement inutile
+- Bcp trop long de faire score algo gén A* >> limite nb générations && nb parkings
+
+A faire:
+- Tester algo génétique avec différents scores >> différents parkings puis comparer
+parkings avec score A* long >> voir meilleure méthode
+
 '''
