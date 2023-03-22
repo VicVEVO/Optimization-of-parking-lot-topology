fcbf3900b5ebde686f27b861e00653d93af3cbd5
Victor <88597698+VicVEVO@users.noreply.github.com>
2023-03-22 22:00:29 +0100

    Update and rename constantes to constantes.py

--- backup/constantes.py
+++ backup/constantes.py
@@ -2,21 +2,31 @@
 Programme secondaire répertoriant les constantes.
 """
 
-
 # Valeurs pour l'algorithme génétique
-N_PARKINGS = 100
-N_GENERATIONS = 50
+N_PARKINGS = 500
+N_GENERATIONS = 1500
 
 PROBA_RANDOM_MUT = 0.3
+PROBA_TRANSPOSE = 0.05
 COEFF_DEVIATION_MUT = 1
+
+SCORE_MAUVAIS = float('inf')
+SCORE_SEUIL = 1
+SCORE_SUP = -100
+
+T_MAX_VOIT = 10 # 1 voiture/10sec
+T_MIN_VOIT = 1 # 1 voiture/sec
+
+a_T = (T_MIN_VOIT-T_MAX_VOIT)/(SCORE_SEUIL-abs(SCORE_SUP))
+b_T = T_MAX_VOIT - a_T*SCORE_SEUIL
 
 
 # Caractéristiques des parkings
 LONGUEUR_PARKING = 15
 LARGEUR_PARKING = 15
 
-I_ENTREE = I_SORTIE = LARGEUR_PARKING//2
-J_ENTREE, J_SORTIE = 0, LONGUEUR_PARKING-1
+COORDS_ENTREES = {(LARGEUR_PARKING//2,0),(0,LONGUEUR_PARKING//2),(0,0)}
+COORDS_SORTIES = {(LARGEUR_PARKING//2,LONGUEUR_PARKING-1)}
 
 
 # Propriétés des simulations 
@@ -33,9 +43,6 @@ NUM_MUR = -1
 NUM_ROUTE = 0
 NUM_PLACE = 1
 NUM_VOITURE = 2
-
-SCORE_SEUIL = 1
-SCORE_MAUVAIS = float('inf')
 
 if __name__ == '__main__':
     print("Ce programme n'est pas destiné à être lancé.")
