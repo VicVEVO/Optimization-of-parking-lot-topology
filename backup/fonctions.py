1963085092c132ece1cfaf970c9580d57b0a980f
Victor <88597698+VicVEVO@users.noreply.github.com>
2023-04-05 10:50:01 +0200

    Update fonctions.py

--- backup/fonctions.py
+++ backup/fonctions.py
@@ -12,23 +12,20 @@ def creationRandomParking(longueur,largeur):
     return np.random.randint(NUM_MUR, NUM_VOITURE, size=(largeur, longueur))
 
 def _parking_cool_test(longueur,largeur):
-    
-    bords = -np.ones(longueur,dtype=int)
 
-    parking = -np.ones((largeur,longueur),dtype=int)
+    parking = np.ones((largeur,longueur),dtype=int)
     
-    for x in range(3,longueur-1):
-        if x%4 == 1 or x%4 == 2:
-            for y in range(2,largeur-2):
-                parking[y,x] = 1
+    for j in range(1,longueur-1):
+        if j%3 == 1:
+            for i in range(1,largeur-1):
+                parking[i,j] = NUM_ROUTE
         else:
-            parking[:,x] = 0 # files vides
-
-    parking[largeur//2],parking[largeur//2-1] = 0,0 # création de la route principale 
-    parking[0,3:-1],parking[1,3:-1],parking[-1,3:-1],parking[-2,3:-1] = 0,0,0,0 # création des routes des fins de files
-
-    parking = np.append(bords,parking).reshape(largeur+1,longueur)
-    parking = np.append(parking,bords).reshape(largeur+2,longueur)
+            parking[:,j] = NUM_PLACE
+    
+    for i in [1,largeur//2,largeur-1]:
+        parking[i,1:longueur-1] = NUM_ROUTE
+    
+    parking[largeur//2,0], parking[largeur//2,longueur-1] = NUM_ROUTE, NUM_ROUTE
 
     return np.array(parking)
         
@@ -65,41 +62,48 @@ def croisementParkings(popParkingsxScores):
     
     return grosseListeParkings
 
-def mutationParkings(popParkingsxScore):
+def mutationParkings(popParkingsxScore,stagnationPop):
     for numParking in range(2,N_PARKINGS):
         (parking,score) = popParkingsxScore[numParking]
         if score > SCORE_SEUIL:
-            mutationRandom(parking)
+            mutationRandom(parking,PROBA_RANDOM_MUT)
+        elif stagnationPop:
+            mutationRandom(parking,PROBA_RANDOM_MUT/20)
         else:
             mutationAjoutBoutRoute(parking)
         popParkingsxScore[numParking] = (parking,score)
     return popParkingsxScore
 
+def stagne(evolScores):
+    return len(evolScores) < SEUIL_STAGNATION or evolScores[-SEUIL_STAGNATION] == evolScores[-1]
+
 def evolutionGenetique():
 
     popParkingsxScores = [(creationRandomParking(LONGUEUR_PARKING,LARGEUR_PARKING),SCORE_MAUVAIS) for _ in range(N_PARKINGS)]
-    EvolScores = np.empty(N_GENERATIONS)
+    evolScores = []
     evolParkings = np.zeros((N_GENERATIONS,LARGEUR_PARKING,LONGUEUR_PARKING))
-
     for _ in range(N_GENERATIONS):
         t1 = time()
         popParkingsxScores = selectionPopParkings(popParkingsxScores)
         meilleurActuel = popParkingsxScores[0]
-
         popParkingsxScores = croisementParkings(popParkingsxScores)
-
-        popParkingsxScores = mutationParkings(popParkingsxScores)
+        stagnationPop = stagne(evolScores)
+        popParkingsxScores = mutationParkings(popParkingsxScores,stagnationPop)
         
         evolParkings[_] = meilleurActuel[0]
-        EvolScores[_] = meilleurActuel[1]
+        evolScores.append(meilleurActuel[1]) 
+
         t2 = time()
         print(meilleurActuel[1],'//',np.round(t2-t1,1),'//',_)
 
-    affichageEvolScore(EvolScores)
-    diagrammeDispersionScores(np.array(popParkingsxScores)[:,1])
+    #affichageEvolScore(np.array(evolScores))
+    #diagrammeDispersionScores(np.array(popParkingsxScores)[:,1])
+    
     return evolParkings,popParkingsxScores
 
 if __name__ == '__main__':
-    p = _parking_cool_test(LONGUEUR_PARKING,LARGEUR_PARKING)
-    print(score(p))
+    p1 = _parking_cool_test(LONGUEUR_PARKING,LARGEUR_PARKING)
+    affichage(p1)
+    print(score(p1))
+    
     #print("Ce programme n'est pas destiné à être lancé.")
